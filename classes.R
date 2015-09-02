9ea6a9fedd61c3780438aa1c3171a5d6c7b4eb0c
Phillip Rhodes <motley.crue.fan@gmail.com>
2015-09-01 21:32:02 -0400

    more playing with classes and types

--- classes.R
+++ classes.R
@@ -25,7 +25,28 @@ writeLines( class(1:5))
 # get details about the internals of types and what-not
 .Machine
 
+# characters, too!
+writeLines( class( c( "She", "sells", "seashells", "on", "the", "shore")))
 
 
+gender <- factor( c( "male", "female", "female", "male", "female" ))
+
+# factors for storing categorical data
+writeLines( toString( gender ) )
+writeLines( class( gender ))
+writeLines( levels(gender))
+writeLines( toString( nlevels(gender) ))
+writeLines( toString( as.integer(gender) ))
+
+# look at the size difference between a vector of characters and a vector of facors
+gender_char <- sample( c( "female", "male"), 1000, replace = TRUE)
+gender_fac <- as.factor( gender_char )
+
+writeLines( toString( object.size(gender_char)))
+
+writeLines( toString( object.size( gender_fac )))
+
+# raw for raw bytes (in hex)
+writeLines( toString( as.raw( 1:17 ) ) )
 
 
